0ef4779ff06be9bc97d2924000e35207f8e07192
khanhlinh2404 <buikhanhlinh028@gmail.com>
2025-01-02 22:15:53 +0700

    CREATE_PROJECT

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BuiKhanhLinh_1083_2425_BaiThi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
